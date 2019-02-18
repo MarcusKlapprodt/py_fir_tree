e7f6afaa6759b472eeeccc4def181ee3ccd23291
marcusklapprodt <taus_i_melek@yahoo.de>
2019-02-18 12:32:57 +0100

    added some unrelated code

--- firtree.py
+++ firtree.py
@@ -1,7 +1,8 @@
 # Create a firtree with asterisks
 
 # Ask for the user input
-initialcounter = int(input("How many branches?"))
+initialcounter = 3
+# int(input("How many branches?"))
 
 # Set up the variables
 branches = initialcounter
@@ -24,3 +25,22 @@ for trunk in range(treetrunk):
 print(" ")
 print(" ")
 print("season's greetings!")
+
+import queue
+list = [1,2,3,4]
+q = queue.PriorityQueue()
+
+q.put((10,"Halo"))
+q.put((15,"Mars"))
+q.put((5,"Phobos"))
+print(q.get())
+
+d = {}
+text = "A A A A A A B B B B B B B B B B B D D D D D D D F F F F"
+for word in text.split(" "):
+    if word in d:
+        d[word] = d[word] +1
+    else:
+        d[word] = 1
+
+print(d)
